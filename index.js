c141a02ee046712b006dc0f1c1bc3a5d001a1711
Zac Orndorff <zac.orndorff@otus.com>
2021-12-03 09:30:13 -0600

    updates

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const program = new Command();
 
 program
   .version('0.0.1')
-  .argument('<file>')
-  .argument('-d, --day <daytoprocess>', 'day of the advent')
+  .option('<inputFile>')
+  .option('-d, --day <daytoprocess>', 'day of the advent')
   .argument('-p, --part <part>', 'Which part to process 1, or 2')
   .option('--debug', 'enable debugging')
   .action((file, options, command) => {
@@ -16,7 +16,7 @@ program
     console.log(`executing advent day ${file}`);
 
     const input = fs.readFileSync(path.join(__dirname, file)).toString().split('\n');
-    const adventProcessor = require(path.join(__dirname,`day_${options.day}`));
+    const adventProcessor = require(path.join(__dirname, 'src', `day_${options.day}`));
     adventProcessor.process(input);
   })
   .parse();
